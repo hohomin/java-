dc817b6e83072cc479e28d53972dd83629a139a7
hohomin <mathsm@163.com>
2017-08-01 09:41:52 +0800

    test02
    二进制转化为十进制

---
+++ Test02.java
@@ -0,0 +1,20 @@
+package com.minmin.www;
+
+public class Test02 {
+ 
+	/**
+	 * 计算（11111111）2=（______）10
+	 */
+	public static void main(String[] args) {
+		System.out.println(Math.pow(2, 7)+
+				           Math.pow(2, 6)+
+				           Math.pow(2, 5)+
+				           Math.pow(2, 4)+
+				           Math.pow(2, 3)+
+				           Math.pow(2, 2)+
+				           Math.pow(2, 1)+
+				           Math.pow(2, 0));
+		System.out.println(Math.pow(2, 8)-1);
+	}
+
+}
